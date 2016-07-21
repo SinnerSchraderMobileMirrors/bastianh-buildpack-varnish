b6a7ebb74d81fbc1d1498dbe7f8d2c30304ba697
Bastian Hoyer <dafire@gmail.com>
2016-07-21 21:54:04 +0200

    test

--- .profile.d/varnish-defaults.sh
+++ .profile.d/varnish-defaults.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 export LD_LIBRARY_PATH="$HOME/bin/varnish/lib/varnish/:$LD_LIBRARY_PATH"
 export PATH="$PATH:$HOME/bin/varnish/bin:$HOME/bin/varnish/sbin"
 echo "*** VARNISH DEFAULTS ***"
